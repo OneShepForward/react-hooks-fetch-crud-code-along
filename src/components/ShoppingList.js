0f898487da168d1f96f8a436a8bb82e655ad92dd
Nicholas Shephard <shephardn@gmail.com>
2021-12-01 17:39:09 -0600

    commit tree 87725ed7a0c0d69c6d08ed971f7f19213f5f896a

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,8 +7,42 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
+  const API_URL = "http://localhost:4000/items"
+  
+  useEffect(() => {
+      fetch(API_URL)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        setItems(data);
+      })
+  }, [])
+  
   function handleCategoryChange(category) {
     setSelectedCategory(category);
+  }
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+    console.log("In Shopping List:", newItem);
+  }
+
+  function handleAddToCart(updatedItem) {
+      const updatedItems = items.map((item) => {
+        if (item.id === updatedItem.id) {
+          return updatedItem;
+        } else {
+          return item;
+        }
+      })
+    setItems(updatedItems);
+  }
+
+  function handleDeleteItem(deletedItem) {
+    
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    console.log(updatedItems);
+    setItems(updatedItems);
   }
 
   const itemsToDisplay = items.filter((item) => {
@@ -19,14 +53,19 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm onAddItem={handleAddItem}/>
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+          key={item.id} 
+          item={item} 
+          onAddToCart={handleAddToCart} 
+          onDeleteClick={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
